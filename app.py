181aff54e36a12920d4021dda7f800cc5882027e
Atharv Attri <atharv260107@gmail.com>
2021-01-27 17:36:55 +0000

    basic layout

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask_assets import Environment, Bundle
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+import subprocess
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+assets = Environment()
+assets.init_app(app)
+
+css = Bundle('src/css/*.css', filters='postcss', output='dist/css/main.css')
+assets.register('css', css)
+
+@app.route("/")
+def home():
+    return render_template("index.html") 
